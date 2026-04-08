6a18455ee1f0e27cd01e911563da946b8237e81d
Shreeraksha Kulal <shreeraksha8147@gmail.com>
2026-04-08 18:12:40 +0530

    Update dup.java

--- dup.java
+++ dup.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i=1 // Points to last unique element
+        int i=1 // First element is always unique
         
         for (int j = 1; j < nums.length; j++) {
             if (nums[j] != nums[i]) {
